9743f990faa464204873c49fc60102b0bd3c6bda
Huseyin Uztetik <husuzttemu@gmail.com>
2019-02-25 01:02:48 +0300

    adding test  GET todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,18 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: 'First test todo'
+},
+{
+    text: 'Second test todo'
+}];
+
 beforeEach((done)=> {
     Todo.remove({}).then(()=> {
+        Todo.insertMany(todos);
+        //done();
+    }).then(() =>{
         done();
     })
 })
@@ -26,7 +36,7 @@ describe('POST/todos',() => {
                 return done(err);
             }
 
-            Todo.find().then((todos) => {
+            Todo.find({text}).then((todos) => {
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -49,7 +59,7 @@ describe('POST/todos',() => {
             }
 
             Todo.find().then((todos) => {
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch((err) => {
                 done(err);
@@ -58,3 +68,16 @@ describe('POST/todos',() => {
 
     });
 });
+
+describe('GET todos',()=> {
+    test('should get all todos',(done)=> {
+
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
+    });
+})
